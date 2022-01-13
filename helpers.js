fe48bd1ff36339d1e2c3aa242574904266c77a9b
Devin Checa <devincheca@gmail.com>
2022-01-12 21:04:30 -0800

    still working through this

--- helpers.js
+++ helpers.js
@@ -28,3 +28,26 @@ function navigateTo(div) {
     toggleArrow('samplesSpan');
   }, 500);
 }())
+function interObserve(func, options) {
+  try {
+    return new IntersectionObserver(func, options);
+  } catch(error) {
+    return func();
+  }
+}
+(function initScrollObserver() {
+  const spans = {
+    'educationSpan': 'education'
+  };
+  const observer = interObserve(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const id = spans[entry.target.id];
+        w3.toggleShow(`#${id}`);
+        toggleArrow(`#${entry.target.id}Span`);
+      }
+    });
+  }, { threshold: 0.5 });
+  const edu = document.querySelector('#educationSpan');
+  observer.observe(edu);
+}())
